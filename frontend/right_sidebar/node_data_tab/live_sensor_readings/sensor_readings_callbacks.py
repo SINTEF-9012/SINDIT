36cd2db6c8c3be1e01230a7593d15df51ea94032
Timo Peter <timoaccs@gmail.com>
2022-06-14 10:25:34 +0200

    Moved frontend config to frontend

--- frontend/right_sidebar/node_data_tab/live_sensor_readings/sensor_readings_callbacks.py
+++ frontend/right_sidebar/node_data_tab/live_sensor_readings/sensor_readings_callbacks.py
@@ -2,11 +2,16 @@ from dash.dependencies import Input, Output, State
 import pandas as pd
 
 from frontend import api_client
+from config import global_config as cfg
 from frontend.app import app
 from frontend.right_sidebar.graph_selector import graph_selector_util
 from frontend.right_sidebar.node_data_tab.live_sensor_readings import sensor_readings_layout
 
 sensor_ID = None
+
+LIVE_SENSOR_DISPLAY_DURATION = cfg.get_float(
+                group=cfg.ConfigGroups.FRONTEND,
+                key='current_timeseries_duration')
 
 print("Initializing sensor callbacks...")
 
@@ -18,7 +23,9 @@ def update_live_sensors(n, selected_el):
 
     if graph_selector_util.get_type(selected_el) == graph_selector_util.SelectedElementTypes.TIMESERIES_INPUT:
         id_uri = graph_selector_util.get_iri(selected_el)
-        data = api_client.get_dataframe(f"/sensors/current_timeseries?sensor_id_uri={id_uri}")
+        data = api_client.get_dataframe(f"/timeseries/current_measurements"
+                                        f"?id_uri={id_uri}"
+                                        f"&duration={LIVE_SENSOR_DISPLAY_DURATION}")
     else:
         print("Trying to visualize timeseries from non-timeseries element...")
         data = pd.DataFrame(columns=['time', 'value'])
--- service/api/endpoints/timeseries_endpoints.py
+++ service/api/endpoints/timeseries_endpoints.py
@@ -3,23 +3,29 @@ from datetime import datetime, timedelta
 import pandas as pd
 
 from service.api.api import app
-from config import global_config as cfg
 from service.exceptions.IdNotFoundException import IdNotFoundException
 from service.timeseries_persistence.TimeseriesPersistenceService import TimeseriesPersistenceService
 
 TS_SERVICE: TimeseriesPersistenceService = TimeseriesPersistenceService.instance()
 
-@app.get("/sensors/current_timeseries")
-def get_current_timeseries(sensor_id_uri: str):
+@app.get("/timeseries/current_measurements")
+def get_current_timeseries(id_uri: str, duration: float):
+    """
+    Queries the current measurements for the given duration up to the current time.
+    :raises IdNotFoundException: If no data is available for that id at the current time
+    :param id_uri:
+    :param duration: timespan to query in seconds
+    :return:
+    """
     try:
-        df = TS_SERVICE.read_period_to_dataframe(
-            id_uri=sensor_id_uri,
-            begin_time=datetime.now() - timedelta(seconds=cfg.get_int(
-                group=cfg.ConfigGroups.API,
-                key='current_timeseries_duration')),
+        readings_df = TS_SERVICE.read_period_to_dataframe(
+            id_uri=id_uri,
+            begin_time=datetime.now() - timedelta(seconds=duration),
             end_time=datetime.now()
         )
 
-        return df.to_json(date_format='iso')
+        return readings_df.to_json(date_format='iso')
     except IdNotFoundException:
         return pd.DataFrame(columns=['time', 'value'])
+
+
