8011ae153f972db635a2ad8775abb4ff0381d84a
Timo Peter <timoaccs@gmail.com>
2022-07-15 09:12:39 +0000

    Code cleanup

--- dtwin/__init__.py
+++
@@ -1,7 +0,0 @@
-from datetime import date as _date
-__author__ = "SINTEF Digital"
-__copyright__ = "{}-{}, {}".format(2021, _date.today().year, __author__)
-__email__ = "@sintef.no"
-__license__ = "free"
-__package__ = "dtwin"
-__version__ = "0.0.1"
